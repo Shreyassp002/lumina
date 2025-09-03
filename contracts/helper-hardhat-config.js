dd8fae250c47616e993224a997fa794f08192ab8
Shreyas Patil <shreyassp002@gmail.com>
2025-09-03 13:50:10 +0530

    added rpc to network config

--- contracts/helper-hardhat-config.js
+++ contracts/helper-hardhat-config.js
@@ -4,14 +4,20 @@ const networkConfig = {
     11155111: {
         name: "sepolia",
     },
+    50311: {
+        name: "somniaTestnet",
+        blockConfirmations: 3,
+    },
     31337: {
         name: "hardhat",
     },
 }
 
 const developmentChains = ["hardhat", "localhost"]
+const testnetChains = ["sepolia", "somniaTestnet"]
 
 module.exports = {
     networkConfig,
     developmentChains,
+    testnetChains,
 }
